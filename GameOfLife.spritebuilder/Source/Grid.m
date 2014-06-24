c10114381c79b34d891d54f5f96f47476da1f520
Terrance Griffith <tjgriffi@ucsd.edu>
2014-06-24 13:52:13 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -73,6 +73,13 @@ static const int GRID_COLUMNS = 10;
     
     //invert it's state - kill it if it's alive, bring it to life if it's dead.
     creature.isAlive = !creature.isAlive;
+    if(creature.isAlive)
+    {
+        self.totalAlive++;
+    }
+    else{
+        self.totalAlive--;
+    }
 }
 
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
